43a86a8de759cd13a4c851a40a7c3b3067d1758a
Dintyo <46850979+dintyo@users.noreply.github.com>
2022-11-14 23:02:23 +1000

    changed font to mono & tidy of code

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ console.log("hello world");
 
 let currentScreen = "";
 let calculatedValue = 0;
-let calcArr = [];
+let calcArr = [0];
 let operatorFlag = false;
 
 ///////////////////////////////////////
@@ -32,7 +32,7 @@ const btnPoint = document.querySelector("#btn-num-point");
 const btnModulo = document.querySelector("#btn-num-modulo");
 const btnPlus = document.querySelector("#btn-plus");
 
-const btnClear = document.querySelector("#btn-clear");
+const btnClearAll = document.querySelector("#btn-clear-all");
 const btnDel = document.querySelector("#btn-del");
 const btnEquals = document.querySelector("#btn-equals");
 
@@ -58,35 +58,52 @@ const divide = function (a, b) {
 };
 
 const operate = function (operator, a, b) {
+  let result = 0;
   // do calculation and return a string after the calc
-  if (operator == "+") return add(a, b) + "";
-  if (operator == "-") return subtract(a, b) + "";
-  if (operator == "*") return multiply(a, b) + "";
-  if (operator == "/") return divide(a, b) + "";
+  if (operator == "+") result = add(a, b);
+  if (operator == "-") result = subtract(a, b);
+  if (operator == "*") result = multiply(a, b);
+  if (operator == "/") result = divide(a, b);
 
+  //convert to string so we can use .length
+  result += "";
+  if (result.length > 8) {
+    if (result.includes(".")) {
+      let wholeNumber = result.match(/\d*(?=[.])/) + "";
+
+      if (wholeNumber.length > 8) {
+        wholeNumber = Number(wholeNumber).toExponential(4);
+      }
+      console.log(`whole number = ${wholeNumber}`);
+      console.log(wholeNumber.length);
+      result = parseFloat(result).toFixed(9 - wholeNumber.length);
+    } else {
+      console.log("number was bigger than 8 digits");
+      result = Number(result).toExponential(4);
+    }
+  }
   console.log("operator was not valid");
+  return result;
 };
 
 ///////////////////////////////////////
-// function to update screen when buttons are pushed
+// button functions
 
-const buttonPushed = function (e) {
-  ///////////////////////////////////////
-  // clear is pressed
+///////////////////////////////////////
+// all clear
+const allClearPushed = function () {
+  updateScreen(0);
+  calculatedValue = 0;
+  calcArr = [0];
+  unToggleOperators();
+  operatorFlag = 1;
+  return null;
+};
 
-  if (e.target.textContent === "AC") {
-    output.textContent = "";
-    currentScreen = "";
-    calculatedValue = 0;
-    calcArr = [];
-    unToggleOperators();
-    return null;
-  }
-
-  ///////////////////////////////////////
-  // delete is pressed
-
-  if (e.target.textContent === "C") {
+///////////////////////////////////////
+// clear
+const clearPushed = function () {
+  if (btnClearAll.textContent == "C") {
     // remove the last letter of the string in the calculator
     currentScreen = 0;
     // if there is an element in the array, we already have a result stored for the next calculation, update the number in the calcArr when we hit the delete key
@@ -95,80 +112,88 @@ const buttonPushed = function (e) {
     }
     operatorFlag = true;
     updateScreen(currentScreen);
-    // exit the function
-    return null;
-  }
-  ///////////////////////////////////////
-  // Equals is pressed
-
-  if (e.target.textContent === "=") {
-    // if there are two elements in the calcArray, we should run the calculation
-    if (calcArr.length === 2) {
-      calcArr.push(currentScreen);
-      const result = operate(calcArr[1], calcArr[0], calcArr[2]);
-      updateScreen(result);
-      calcArr = [];
-      calcArr.push(result);
-    }
-    operatorFlag = true;
-    return null;
-  }
-  ///////////////////////////////////////
-  // an operator is pressed
-  if (
-    e.target.textContent == "+" ||
-    e.target.textContent == "-" ||
-    e.target.textContent == "/" ||
-    e.target.textContent == "*"
-  ) {
-    // if a second operator is pressed twice
-    if (operatorFlag) {
-      unToggleOperators();
-      calcArr[1] = e.target.textContent;
-      e.target.classList.add("operator-pressed");
-      return;
-    }
-
-    operatorFlag = true;
-    if (calcArr.length == 2) {
-      calcArr.push(output.textContent);
-      console.log(calcArr);
-      const result = operate(calcArr[1], calcArr[0], calcArr[2]);
-      updateScreen(result);
-      calcArr = [];
-      console.log("did this happen twice?");
-      calcArr.push(result);
-      calcArr.push(e.target.textContent);
-      console.log(calcArr);
-    } else if (calcArr.length == 1) {
-      calcArr[0] = currentScreen;
-      calcArr.push(e.target.textContent);
-    } else {
-      calcArr.push(currentScreen);
-      calcArr.push(e.target.textContent);
-    }
-    e.target.classList.add("operator-pressed");
+    toggleClear(false);
+    btnClearAll.textContent = "AC";
+    console.log("did we get here?");
   } else {
-    // add whatever is pressed to the screen
+    // all clear
+    allClearPushed();
+  }
+};
 
-    if (!operatorFlag) {
-      // if we don't have an operator selected
-      currentScreen += e.target.textContent;
-      updateScreen(currentScreen);
-      operatorFlag = false;
-      unToggleOperators();
-    } else {
-      // we have an operator selected, so clear the screen
-      // by setting the new number to be the button pressed
-      currentScreen = e.target.textContent;
-      updateScreen(currentScreen);
-      operatorFlag = false;
-      unToggleOperators();
-    }
+///////////////////////////////////////
+// Equals is pressed
+
+const equalsPushed = function (e) {
+  // if there are two elements in the calcArray, we should run the calculation
+  if (calcArr.length === 2) {
+    calcArr.push(currentScreen);
+    const result = operate(calcArr[1], calcArr[0], calcArr[2]);
+    updateScreen(result);
+    calcArr = [];
+    calcArr.push(result);
+  }
+  unToggleOperators();
+  operatorFlag = true;
+};
+
+///////////////////////////////////////
+// an operator is pressed
+
+const operatorPushed = function (e) {
+  // if a second operator is pressed twice
+  if (operatorFlag) {
+    unToggleOperators();
+    calcArr[1] = e.target.textContent;
+    e.target.classList.add("operator-pressed");
+    return;
   }
 
-  console.log(calcArr);
+  operatorFlag = true;
+  if (calcArr.length == 2) {
+    calcArr.push(output.textContent);
+    console.log(calcArr);
+    const result = operate(calcArr[1], calcArr[0], calcArr[2]);
+    updateScreen(result);
+    calcArr = [];
+    console.log("did this happen twice?");
+    calcArr.push(result);
+    calcArr.push(e.target.textContent);
+    console.log(calcArr);
+  } else if (calcArr.length == 1) {
+    calcArr[0] = currentScreen;
+    calcArr.push(e.target.textContent);
+  } else {
+    calcArr.push(currentScreen);
+    calcArr.push(e.target.textContent);
+  }
+  e.target.classList.add("operator-pressed");
 };
+
+const numButtonPushed = function (e) {
+  //if we have filled the screen and we don't have an
+  //operator selected, do nothing
+  if (currentScreen.length > 9 && !operatorFlag) return;
+  // we toggle the clear on any time a number is pushed
+  toggleClear(1);
+  if (!operatorFlag) {
+    // if we don't have an operator selected
+    currentScreen += e.target.textContent;
+    updateScreen(currentScreen);
+    operatorFlag = false;
+    unToggleOperators();
+  } else {
+    // we have an operator selected, so clear the screen
+    // by setting the new number to be the button pressed
+    currentScreen = e.target.textContent;
+    updateScreen(currentScreen);
+    operatorFlag = false;
+    unToggleOperators();
+  }
+};
+
+///////////////////////////////////////
+// helper functions
 
 const unToggleOperators = function () {
   btnDivide.classList.remove("operator-pressed");
@@ -182,7 +207,23 @@ const updateScreen = function (str) {
   output.textContent = currentScreen;
 };
 
-const allButtons = document.querySelectorAll(".btn-calculator");
-allButtons.forEach((button) => {
-  button.addEventListener("click", buttonPushed);
+const toggleClear = function (bool) {
+  bool ? (btnClearAll.textContent = "C") : (btnClearAll.textContent = "AC");
+  console.log(btnClearAll.textContent);
+};
+
+///////////////////////////////////////
+// button event listeners
+
+const numButtons = document.querySelectorAll(".btn-num");
+numButtons.forEach((button) => {
+  button.addEventListener("click", numButtonPushed);
 });
+
+btnMinus.addEventListener("click", operatorPushed);
+btnDivide.addEventListener("click", operatorPushed);
+btnPlus.addEventListener("click", operatorPushed);
+btnMultiply.addEventListener("click", operatorPushed);
+
+btnEquals.addEventListener("click", equalsPushed);
+btnClearAll.addEventListener("click", clearPushed);
